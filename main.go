4a3d36a207661f9d11cf88839c76b02b8deda063
Ram Bhajan Mishra <rbm897@gmail.com>
2024-10-05 13:40:33 +0530

    fixing types

--- main.go
+++ main.go
@@ -3,11 +3,12 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
-	num1 := os.Getenv("INPUT_NUM1")
-	num2 := os.Getenv("INPUT_NUM2")
+	num1, _ := strconv.Atoi(os.Getenv("INPUT_NUM1"))
+	num2, _ := strconv.Atoi(os.Getenv("INPUT_NUM2"))
 	sum := num1 + num2
-	fmt.Printf("::set-output name=sum::%s", sum)
+	fmt.Printf("sum=%d >> $GITHUB_OUTPUT", sum)
 }
