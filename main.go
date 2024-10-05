0d4c4e06c646c6271ff66e3e33eca89841ecaa2c
Ram Bhajan Mishra <rbm897@gmail.com>
2024-10-05 13:30:34 +0530

    testing output

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 )
 
 func main() {
-	name := os.Getenv("INPUT_NAME")
-	fmt.Printf("Name = %s", name)
+	num1 := os.Getenv("INPUT_NUM1")
+	num2 := os.Getenv("INPUT_NUM2")
+	sum := num1 + num2
+	fmt.Printf("::set-output name=sum::%s", sum)
 }
