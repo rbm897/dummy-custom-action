7f6cf795821a6cf653aeb1fc2501fed741f97610
Ram Bhajan Mishra <rbm897@gmail.com>
2024-10-05 12:49:24 +0530

    Added input

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello from Dummy Custom Actions")
+	name := os.Getenv("INPUT_name")
+	fmt.Printf("Name = %s", name)
 }
